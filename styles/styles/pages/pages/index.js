901957f8348bb1baf90f65ef1a374bdc8e2b7639
feras204 <feras_204@hotmail.com>
2026-07-11 15:13:00 +0300

    Delete styles/styles directory

--- styles/styles/pages/pages/index.js
+++
@@ -1,5 +0,0 @@
-const stored = localStorage.getItem(STORAGE_KEY);
-if (stored) {
-  const l = JSON.parse(stored);
-localStorage.setItem(STORAGE_KEY, JSON.stringify(data));
-localStorage.setItem(STORAGE_KEY, JSON.stringify(DEFAULT_DATA));
